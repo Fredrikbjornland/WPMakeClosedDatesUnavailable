66ccf4fa7e4750d7da544b795685a250738a0d97
Fredrik bjornland <43269579+Fredrikbjornland@users.noreply.github.com>
2020-10-20 23:10:50 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+//Make sunday and monday unavailable
+gform.addFilter('gform_datepicker_options_pre_init', function (optionsObj, formId, fieldId) {
+    if (formId == 1 && fieldId == 7) {
+		console.log("etset")
+        optionsObj.firstDay = 1;
+        optionsObj.beforeShowDay = function (date) {
+                day = date.getDay();
+			//0 = sunday, 1=monday
+            return [(day != 0 && day != 1)];
+        };
+    }
+ 
+    return optionsObj;
+});
+//If its past 19:00, make todays date unavailable
+gform.addFilter( 'gform_datepicker_options_pre_init', function( optionsObj, formId, fieldId ) {
+	if ( fieldId == 7 ) {
+		var d = new Date();
+		var n = d.getHours();
+		var firstAllowedDate = 0
+		if (n > 19) {
+			firstAllowedDate = 1
+		}
+		optionsObj.minDate = firstAllowedDate;
+	}
+	
+	return optionsObj;
+});
